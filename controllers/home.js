2b52b0d1e7b0dc04c58ef5ade15493bc31e0eff2
sam-c14 <samuelaffah14@gmail.com>
2023-05-01 16:28:28 +0100

    Express added and configured

---
+++ controllers/home.js
@@ -0,0 +1,7 @@
+const getProducts = (req, res) => {
+  res.send("Hello World");
+};
+
+module.exports = {
+  getProducts,
+};
--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const app = express();
+const homeRoutes = require("./routes/home");
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
-app.listen(() => {
-  console.log("Server is listening at port 5000");
+app.use(express.json());
+app.use("/api/v1", homeRoutes);
+// app.get("/", (req, res) => {
+//   res.send("Hello World");
+// });
+const port = 5000;
+app.listen(port, () => {
+  console.log("Server is listening at port 5000...");
 });
