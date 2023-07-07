1998395adf859d086bacc83c02e7d16676c42160
sam-c14 <samuelaffah14@gmail.com>
2023-07-07 20:39:08 +0100

    Changes made to the simple auth system

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,18 @@
 const Auth = require("../models/auth");
 
 const loginUser = async (req, res) => {
-  const user = await Auth.findOne(req.body).exec();
+  try {
+    const user = await Auth.findOne(req.body).exec();
+    if (user === null) {
+      res.status(404).json({
+        errorMsg: "User not found",
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      errMsg: "There was an error processing the request.",
+    });
+  }
   res.status(200).json({ user });
 };
 const registerUser = async (req, res) => {
