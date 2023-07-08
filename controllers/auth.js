ef8fdd4b0263c2bafbdfe56b539b43293f90f4fc
sam-c14 <samuelaffah14@gmail.com>
2023-07-08 12:58:45 +0100

    Status Text bug resolved

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,18 +4,14 @@ const loginUser = async (req, res) => {
   const user = await Auth.findOne(req.body).exec();
   try {
     if (user === null) {
-      return res
-        .status(404)
-        .statusText("The user is has not been registered")
-        .json({ user });
+      res.statusMessage =
+        "The user could not be found, Please Register to be able to login";
+      return res.status(404).json({ user });
     }
   } catch (error) {
-    return res
-      .status(500)
-      .statusText(
-        "There was an error processing the request from the server, Try Again"
-      )
-      .json({ user });
+    res.statusMessage =
+      "There was an error processing the request from the server, Please Try Again";
+    return res.status(500).json({ user });
   }
   res.status(200).json({ user });
 };
