97aeabc00291ecf310eb8bda779cce4e9f8463c8
sam-c14 <samuelaffah14@gmail.com>
2023-07-08 09:21:49 +0100

    Bug in Auth Schema fixed

--- models/auth.js
+++ models/auth.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 // const passportLocalMongoose = require("passport-local-mongoose");
 var UserSchema = new Schema({
-  username: String,
+  email: String,
   password: String,
 });
 
