4f6f1f5762810da9a2c77ccf72864dc73d6338f0
sam-c14 <samuelaffah14@gmail.com>
2023-08-19 01:49:07 +0100

    Test token

--- middlewares/auth/authJwt.js
+++ middlewares/auth/authJwt.js
@@ -6,7 +6,7 @@ const Role = db.role;
 
 verifyToken = async (req, res, next) => {
   // let token = req.session.token;
-  const token = req.header("Authorization").split(" ");
+  const token = req.header("Authorization").split(" ")[1];
 
   if (!token) {
     return res.status(403).send({ message: "No token provided!" });
